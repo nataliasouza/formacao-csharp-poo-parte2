e37a3c7f56b9832bf064a0f84aac66561729545d
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-07-18 16:31:49 -0300

    Métodos de acesso e propriedade

--- screenSound02/screenSound02/Musica.cs
+++ screenSound02/screenSound02/Musica.cs
@@ -1,28 +1,32 @@
 ﻿
 public class Musica
 {    
-    public string Nome;
-    public string Artista;
-    public int Duracao;
-    public bool Disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => 
+        $"Música: {Nome}, pertence à banda {Artista}.";
+
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine("\n================================\n");
+        Console.WriteLine("\n======================================================================================\n");
         Console.WriteLine($"Nome: {Nome}");
         Console.WriteLine($"Artista: {Artista}");
-        Console.WriteLine($"Duração: {Duracao}");
-        Console.WriteLine($"Disponível: {Disponivel}");
-        
+        Console.WriteLine($"Duração: {Duracao}");      
 
         if (Disponivel)
         {
             Console.WriteLine("Disponível no plano.\n");
-            Console.WriteLine("================================");
+           
         } else
         {
             Console.WriteLine("Adquira o plano premium.\n");
-            Console.WriteLine("================================");
+            
         }
+
+        Console.WriteLine($"Descricao Resumida: {DescricaoResumida}");
+        Console.WriteLine("\n======================================================================================");
     }
 }
