ed43c369c1d5789be0f91f24b840ee7382cde63f
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-07-20 22:11:18 -0300

    Criação da classe Banda

---
+++ screenSound02/screenSound02/Banda.cs
@@ -0,0 +1,25 @@
+﻿
+public class Banda
+{
+    private List<Album> albuns = new List<Album>();
+    public string Nome { get; }
+
+    public Banda(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void AdicionarAlbum(Album album)
+    {
+        albuns.Add(album);
+    }
+
+    public void ExibirDiscografia()
+    {
+        Console.WriteLine($"\nDiscografia da Banda: {Nome}");
+        foreach (Album album in albuns)
+        {
+            Console.WriteLine($"Álbum: {album.Nome} (Duração: {album.DuracaoTotal})");
+        }
+    }
+}
