846436bc7feeab52ec5fa731afe82306b5fb7290
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-07-20 22:10:44 -0300

    Criação da classe Genero

---
+++ screenSound02/screenSound02/Genero.cs
@@ -0,0 +1,32 @@
+﻿
+public class Genero
+{
+    private List<Musica> listaDosGeneros = new List<Musica>();
+    public string Nome { get;}
+
+    public Genero(string nome)
+    {
+        Nome = nome;
+    }
+    public Genero()
+    {
+            
+    }
+
+    public void AdicionarGeneroDaMusica(Musica genero)
+    {
+        listaDosGeneros.Add(genero);
+    }
+
+    public void ExibirMusicasPorGenero()
+    {
+        Console.WriteLine("\n======================================================================================\n");
+        Console.WriteLine($"\tLista de músicas de gênero: {Nome} ");
+        Console.WriteLine("\n======================================================================================\n");
+
+        foreach (var musicaPorGenero in listaDosGeneros)
+        {
+            Console.WriteLine($"Música: {musicaPorGenero.Nome} - Artista: {musicaPorGenero.Artista.Nome}");
+        }        
+    }
+}
