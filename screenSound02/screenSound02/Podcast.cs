52a9ce645200413a9a04c464664f1d663091778e
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-07-22 22:16:25 -0300

    Classe Podcast

---
+++ screenSound02/screenSound02/Podcast.cs
@@ -0,0 +1,32 @@
+﻿public class Podcast
+{
+    private List<Episodio> episodios = new();   
+
+    public string Nome { get; }
+    public string Host { get; }
+
+    public Podcast(string nome, string host)
+    {
+        Nome = nome;
+        Host = host;
+    }
+    public int TotalEpisodios => episodios.Count;
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine("\n======================================================================================\n");
+        Console.WriteLine($"\tPODCAST: {Nome} |  Apresentado por [ {Host} ]\n");
+        Console.WriteLine("======================================================================================\n");
+
+        foreach (Episodio episodio in episodios.OrderBy(e => e.Ordem))
+        {
+            Console.WriteLine(episodio.Resumo);
+        }
+        Console.WriteLine($"\n\nTotal de episódios: {TotalEpisodios}.\n\n");
+    }
+}
